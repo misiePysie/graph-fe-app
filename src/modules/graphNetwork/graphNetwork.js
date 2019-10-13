8b5f2b5a959a206c9bde4baceff3b499a9964b9e
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-10-13 16:47:17 +0200

    replace id with path

--- src/modules/graphNetwork/graphNetwork.js
+++ src/modules/graphNetwork/graphNetwork.js
@@ -4,18 +4,18 @@ import http from "../../services/httpService";
 
 // temporary mock data
 const nodesMockData = [
-  {id: 1, label: 'Node 1'},
-  {id: 2, label: 'Node 2'},
-  {id: 3, label: 'Node 3'},
-  {id: 4, label: 'Node 4'},
-  {id: 5, label: 'Node 5'}
+  {id:'./src/1', label: 'Node 1'},
+  {id:'./src/2', label: 'Node 2'},
+  {id:'./src/3', label: 'Node 3'},
+  {id:'./src/4', label: 'Node 4'},
+  {id:'./src/5', label: 'Node 5'}
 ]
 const edgesMockData = [
-  {from: 1, to: 3},
-  {from: 1, to: 2},
-  {from: 2, to: 4},
-  {from: 2, to: 5},
-  {from: 3, to: 3}
+  {from: './src/1', to: './src/3'},
+  {from: './src/1', to: './src/2'},
+  {from: './src/2', to: './src/4'},
+  {from: './src/2', to: './src/5'},
+  {from: './src/3', to: './src/3'}
 ]
 
 async function getProjectStructureData(){
